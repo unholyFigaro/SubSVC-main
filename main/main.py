4bfb505ea4143eb806b1a764d024790c3b3a24a9
unholyFigaro <71033284+unholyFigaro@users.noreply.github.com>
2024-01-17 21:40:23 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ from producer import publish
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@10.244.2.25/main'
 CORS(app)
 
 db = SQLAlchemy(app)
