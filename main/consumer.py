5e09303a54c20ed93a6451401a2b3892cc08c6ac
unholyFigaro <71033284+unholyFigaro@users.noreply.github.com>
2024-01-17 21:31:33 +0300

    Update consumer.py

--- main/consumer.py
+++ main/consumer.py
@@ -1,7 +1,7 @@
 import pika, json
 from main import Product, db
 
-rmq_server = "16.171.196.128"
+rmq_server = "16.171.249.80"
 credentials = pika.PlainCredentials('guest', 'guest')
 parameters = pika.ConnectionParameters(rmq_server, 5672, '/', credentials)
 
